d18a90c07750171df0a9563a83b0a590dc955a71
mnurzhanov <46998363+mnurzhanov@users.noreply.github.com>
2019-01-31 00:39:29 +0600

    Delete 5.js

--- Week-1/JS/5.js
+++
@@ -1,6 +0,0 @@
-let tool = 'marker';
-
-// Use short circuit evaluation to assign  writingUtensil variable below:
-let writingUtensil = tool || 'pen'
-
-console.log(`The ${writingUtensil} is mightier than the sword.`);
